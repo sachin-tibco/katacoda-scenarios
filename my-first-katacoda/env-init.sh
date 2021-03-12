3b776c79f63aed25f775ebb61111601784be9693
sarathor <sarathor@Sachins-2019-MacBook-Pro.local>
2021-03-12 11:43:57 +0530

    Added postgres and worker pull image code in init

--- my-first-katacoda/env-init.sh
+++ my-first-katacoda/env-init.sh
@@ -1,4 +1,6 @@
 git clone https://github.com/dockersamples/example-voting-app.git 
 docker pull python:3.9-slim
 docker pull mcr.microsoft.com/dotnet/core/sdk:2.1
+docker pull mcr.microsoft.com/dotnet/core/runtime:2.1
+docker pull postgres:9.4
 docker pull node:10-slim
